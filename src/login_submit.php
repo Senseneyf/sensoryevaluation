6412f47e8d023e700beee3e8e8ce4eee4243a832
Frank <northley@gmail.com>
2018-02-03 19:41:58 -0800

    Updated login_submit.php to use prepared statments

--- src/login_submit.php
+++ src/login_submit.php
@@ -1,39 +1,34 @@
 <?php
-session_start();
-
+	
+	session_start();
 	if(isset($_POST['username']) and isset($_POST['password'])){
     
 	/* filter username and password */
 	$username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
-
-    /* encrypt password  */
-    $password = sha1($password);
+    $password = sha1(filter_var($_POST['password'], FILTER_SANITIZE_STRING));
+	$user_id = 0;
+	$email = "";
 
 	/* Attempt to connect to db */
-    $connection = mysqli_connect('localhost','root','dbpassword','seneval');
-	if (!$connection){
-		die("Database connect failed" . mysqli_error($connection));
+    $con = new mysqli('localhost','root','blah','dbname');
+	if($con->connect_error){
+		exit("Database connection failed");
 	}
-	
-	/* Query db */
-	$query = "SELECT * FROM `user` WHERE userid='$username' and password='$password'";
-	$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-	$count = mysqli_num_rows($result);
-	
-	/* if credentials match, set session username */
-	if($count == 1){
+	/* Query db with prepared statment */
+	$stmt = $con->prepare("SELECT ID, userid, password, email FROM user WHERE userid= ? and password= ? LIMIT 1");
+	$stmt->bind_param("ss", $username, $password);
+	$stmt->execute();
+	$stmt->bind_result($user_id, $username, $password, $email);
+	$stmt->store_result();
+	/* if credentials match set session username and ID */
+	if($stmt->num_rows == 1){
 		$_SESSION['username']  = $username;
+		$_SESSION['user_id']  = $user_id;
 		/* redirect to index */
 		header("Location: /se/");
 		exit();
 	}
-	else{
-		$msg = "Invalid credentials, please try again.";
-	}
-}
-else{
-	
+	$stmt->close();
 }
 ?>
 
@@ -43,7 +38,7 @@ else{
 </head>
 <body>
 <?php 
-	echo "<div class='center'>".$msg."</div>";
+	echo "<div class='center'>Invalid credentials, please try again.</div>";
 	$referrer = $_SERVER['HTTP_REFERER']; 
 	header ("Refresh: 2;URL='$referrer'");
 ?>
